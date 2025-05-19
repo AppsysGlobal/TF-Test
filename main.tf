c2a5961a5fd06180fb490fcdf02221c5a6208f86
AppsysGlobal <sajith.m@appsysglobal.com>
2025-05-19 17:02:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,11 @@
 provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
+  config_file_profile = "DEFAULT"
 }
 
-resource "oci_core_instance" "vm1" {}
-resource "oci_core_instance" "vm2" {}
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = "ocid1.tenancy.oc1..aaaaaaaasiigfpcj7o4xn6o5r725u5zofb5tfmfb57vzqqsirlnkhg6lpiva"
+}
 
-resource "oci_core_virtual_network" "vcn1" {}
-
-resource "oci_core_subnet" "subnet1" {}
+output "availability_domains" {
+  value = data.oci_identity_availability_domains.ads.availability_domains[*].name
+}
