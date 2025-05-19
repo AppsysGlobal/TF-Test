8643a2b6abc028430c9c0f9dfae88895ad510895
AppsysGlobal <sajith.m@appsysglobal.com>
2025-05-19 17:13:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 provider "oci" {
-  config_file         = "/var/lib/jenkins/.oci/config"
-  config_file_profile = "DEFAULT"
+ tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
 }
 
 data "oci_identity_availability_domains" "ads" {
